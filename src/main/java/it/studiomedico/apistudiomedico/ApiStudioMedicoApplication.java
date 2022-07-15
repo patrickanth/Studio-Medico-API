cf74d864db28b19a56e09e4a5f5693c5578db7c7
patrickanth <patrickanthony979@gmail.com>
2022-07-15 11:04:09 +0200

    start basic application

---
+++ src/main/java/it/studiomedico/apistudiomedico/ApiStudioMedicoApplication.java
@@ -0,0 +1,13 @@
+package it.studiomedico.apistudiomedico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiStudioMedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiStudioMedicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/studiomedico/apistudiomedico/ApiStudioMedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.studiomedico.apistudiomedico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiStudioMedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
