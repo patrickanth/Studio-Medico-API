a9f45824d5b0b2fed76b68d54a12c7f68c4a8bac
Michele Carrillo <michelec1299@gmail.com>
2022-07-26 15:27:36 +0200

    -tolto lombook e inseriti getter, setter e costruttori -inserita relazione uno a molti tra le tabelle prenotazioni e medico

--- src/main/java/it/studiomedico/apistudiomedico/entities/Segretario.java
+++ src/main/java/it/studiomedico/apistudiomedico/entities/Segretario.java
@@ -7,21 +7,64 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.util.List;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "Segretario")
 public class Segretario {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int iDSegretario;
+
     private String name;
+
     private String surname;
+
     @Column(unique = true, name = "emailSegretario",length = 100)
     private String email;
 
 
-    @OneToMany(mappedBy = "idSegretario")
+    @OneToMany(mappedBy = "Segretario")
     private List<Medico> medici;
 
+    public Segretario() {
+    }
+
+    public Segretario(int iDSegretario, String name, String surname, String email) {
+        this.iDSegretario = iDSegretario;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+
+    public int getiDSegretario() {
+        return iDSegretario;
+    }
+
+    public void setiDSegretario(int iDSegretario) {
+        this.iDSegretario = iDSegretario;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
