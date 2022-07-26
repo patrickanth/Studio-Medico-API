e06a02a3675490ed48a1a2154453d4c02e627204
Michele Carrillo <michelec1299@gmail.com>
2022-07-26 16:13:46 +0200

    Revert "-tolto lombook e inseriti getter, setter e costruttori"
    This reverts commit a9f45824d5b0b2fed76b68d54a12c7f68c4a8bac.

--- src/main/java/it/studiomedico/apistudiomedico/entities/Medico.java
+++ src/main/java/it/studiomedico/apistudiomedico/entities/Medico.java
@@ -7,20 +7,20 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "Medico")
 public class Medico {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private int idMedico;
-
     private String name;
-
     private String surname;
-
     @Column(unique = true, name = "emailMedico",length = 100)
     private String email;
+    private int idSegretario;
 
     @ManyToOne
     private Segretario segretario;
@@ -28,48 +28,5 @@ public class Medico {
     @OneToMany(mappedBy = "medico")
     private List<Paziente> pazienti;
 
-    @OneToMany(mappedBy = "medico")
-    private List<Prenotazioni> prenotazioni;
 
-    public Medico() {
-    }
-
-    public Medico(int idMedico, String name, String surname, String email) {
-        this.idMedico = idMedico;
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-    }
-
-    public int getIdMedico() {
-        return idMedico;
-    }
-
-    public void setIdMedico(int idMedico) {
-        this.idMedico = idMedico;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
--- src/main/java/it/studiomedico/apistudiomedico/entities/Paziente.java
+++ src/main/java/it/studiomedico/apistudiomedico/entities/Paziente.java
@@ -7,66 +7,24 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "Paziente ")
 public class Paziente {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private int idPaziente;
-
     private String name;
-
     private String surname;
-
     @Column(unique = true, name = "emailPaziente",length = 100)
     private String email;
 
     @ManyToOne
     private Medico medico;
 
-    @OneToMany(mappedBy = "Paziente")
+    @OneToMany(mappedBy = "idPaziente")
     private List<Prenotazioni> prenotazioni;
 
-    public Paziente() {
-    }
-
-    public Paziente(int idPaziente, String name, String surname, String email) {
-        this.idPaziente = idPaziente;
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-    }
-
-    public int getIdPaziente() {
-        return idPaziente;
-    }
-
-    public void setIdPaziente(int idPaziente) {
-        this.idPaziente = idPaziente;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
