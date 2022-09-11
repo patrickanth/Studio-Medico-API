2e6a5cc92c092b9f5e798c8e03909b00a53b14f0
patrickanth <patrickanthony979@gmail.com>
2022-09-11 10:43:08 +0200

    Fix CRUD Prenotazione

--- src/main/java/it/studiomedico/apistudiomedico/controllers/ControllerPrenotazione.java
+++ src/main/java/it/studiomedico/apistudiomedico/controllers/ControllerPrenotazione.java
@@ -5,8 +5,6 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import it.studiomedico.apistudiomedico.entities.Medico;
-import it.studiomedico.apistudiomedico.entities.Paziente;
 import it.studiomedico.apistudiomedico.entities.Prenotazioni;
 import it.studiomedico.apistudiomedico.entitiesDTO.PrenotazioniDTO;
 import it.studiomedico.apistudiomedico.repository.MedicoRepository;
@@ -15,8 +13,8 @@ import it.studiomedico.apistudiomedico.repository.PrenotazioniRepository;
 import it.studiomedico.apistudiomedico.services.MedicoService;
 import it.studiomedico.apistudiomedico.services.PazienteService;
 import it.studiomedico.apistudiomedico.services.PrenotazioneService;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -47,48 +45,64 @@ public class ControllerPrenotazione {
     private MedicoService medicoService;
 
 
-    @ApiOperation(value="crea prenotazione",notes="creates a new prenotazione" )
+    @ApiOperation(value="crea prenotazione",notes="creates a new booking" )
     @ApiResponses({
             @ApiResponse(code=400, message = "BAD DATA INPUT"),
             @ApiResponse(code=500, message = "INTERNAL ERROR FROM SPRING", response = String.class),
             @ApiResponse(code=202, message = "Created")
     })
+
     @PostMapping("/crea-prenotazione/{idPaziente}/{idMedico}")
-    public ResponseEntity<Prenotazioni> addPrenotazioni(@ApiParam(value="The idPaziente of prenotazione")@PathVariable int idPaziente,
+    public ResponseEntity<Prenotazioni> creaPrenotazioni(@ApiParam(value="The idPaziente of prenotazione")@PathVariable int idPaziente,
                                                         @ApiParam(value="The idMedico of prenotazione")@PathVariable int idMedico,
                                                         @ApiParam(value="The prenototazione request body")@RequestBody PrenotazioniDTO prenotazione) {
-        Paziente pazienteDaAssegnare = pazienteRepository.getReferenceById(idPaziente);
-        pazienteService.assegnaPaziente(pazienteDaAssegnare);
-        Medico medicoDaAssegnare = medicoRepository.getReferenceById(idMedico);
-        medicoService.assegnaMedico(medicoDaAssegnare);
-        return prenotazioneService.creaPrenotazioni(prenotazione);
+        if(pazienteRepository.existsById(idPaziente) & medicoRepository.existsById(idMedico)){
+            prenotazione.setDataPrenotazione(prenotazione.getDataPrenotazione());
+            prenotazione.setSedeVisita(prenotazione.getSedeVisita());
+            prenotazione.setIdMedico(idMedico);
+            prenotazione.setIdPaziente(idPaziente);
+            prenotazione.setStatoPrenotazione(prenotazione.getStatoPrenotazione());
+            return prenotazioneService.creaPrenotazioni(prenotazione);
+        }
+        else
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
-    @ApiOperation(value="Lista prenotazione",notes="return a list of prenotazioni in database" )
+    @ApiOperation(value="Lista prenotazione",notes="return a list of bookings in database" )
     @GetMapping("/lista-prenotazioni")
     public List<Prenotazioni> listaPrenotazioni() {
 
         return prenotazioneService.findAll();
     }
+    @ApiOperation(value = "trova prenotazione", notes = "return a single existing booking")
+    @GetMapping("trova-Prenotazione")
+    public Optional<Prenotazioni> trovaSingolaPrenotazione(@ApiParam(value = "The idPrenotazione of prenotazione")
+                                                               @RequestParam int idPrenotazione){
+        return prenotazioneService.trovaPrenotazione(idPrenotazione);
+    }
 
     @ApiOperation(value="modifica prenotazione",notes="edit a prenotazione  in the database" )
     @PutMapping("/modifica-prenotazione")
-    public Prenotazioni addPrenotazioni(@ApiParam(value="The idPaziente of prenotazione")@RequestParam int idPaziente,
+    public ResponseEntity<Prenotazioni> aggiornaPrenotazioni(@ApiParam(value="The idPaziente of prenotazione")@RequestParam int idPaziente,
                                         @ApiParam(value="The idMedico of prenotazione")@RequestParam int idMedico,
                                         @ApiParam(value="The idPrenotazione of prenotazione")@RequestParam int idPrenotazione,
                                         @ApiParam(value="The prenototazione request body")@RequestBody PrenotazioniDTO prenotazione) {
-        Prenotazioni prenotazioneModificata = prenotazioniRepository.getReferenceById(idPrenotazione);
-        Paziente pazienteDaAssegnare = pazienteRepository.getReferenceById(idPaziente);
-        pazienteService.assegnaPaziente(pazienteDaAssegnare);
-        Medico medicoDaAssegnare = medicoRepository.getReferenceById(idMedico);
-        medicoService.assegnaMedico(medicoDaAssegnare);
-        return prenotazioniRepository.saveAndFlush(prenotazioneModificata);
+        if (pazienteRepository.existsById(idPaziente) & medicoRepository.existsById(idMedico) && prenotazioniRepository.existsById(idPrenotazione)) {
+            prenotazione.setDataPrenotazione(prenotazione.getDataPrenotazione());
+            prenotazione.setSedeVisita(prenotazione.getSedeVisita());
+            prenotazione.setIdMedico(idMedico);
+            prenotazione.setIdPaziente(idPaziente);
+            prenotazione.setStatoPrenotazione(prenotazione.getStatoPrenotazione());
+            return prenotazioneService.aggiornaPrenotazioni(prenotazione);
+        }
+        else
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
-    @ApiOperation(value="Cancella prenotazione",notes="Delete a prenotazione from the database" )
+    @ApiOperation(value="Cancella prenotazione",notes="Delete a booking from the database" )
     @DeleteMapping("/cancella-prenotazione")
-    public void cancellaPaziente(@ApiParam(value="The idPrenotazione of prenotazione")@RequestParam int idPrenotazione) {
-        prenotazioneService.deleteById(idPrenotazione);
+    public void cancellaPrenotazione(@ApiParam(value="The idPrenotazione of prenotazione")@RequestParam int idPrenotazione) {
+        prenotazioneService.cancellaPrenotazione(idPrenotazione);
     }
 }
 
--- src/main/java/it/studiomedico/apistudiomedico/services/PrenotazioneService.java
+++ src/main/java/it/studiomedico/apistudiomedico/services/PrenotazioneService.java
@@ -1,9 +1,6 @@
 package it.studiomedico.apistudiomedico.services;
 
-import it.studiomedico.apistudiomedico.entities.Medico;
-import it.studiomedico.apistudiomedico.entities.Paziente;
 import it.studiomedico.apistudiomedico.entities.Prenotazioni;
-import it.studiomedico.apistudiomedico.entitiesDTO.MedicoDTO;
 import it.studiomedico.apistudiomedico.entitiesDTO.PrenotazioniDTO;
 import it.studiomedico.apistudiomedico.repository.MedicoRepository;
 import it.studiomedico.apistudiomedico.repository.PazienteRepository;
@@ -13,10 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PrenotazioneService {
@@ -46,7 +42,19 @@ public class PrenotazioneService {
         return  prenotazioniRepository.findAll();
     }
 
-    public void deleteById(int idPrenotazione) {
+    public Optional<Prenotazioni> trovaPrenotazione(int idPrenotazione){
+            return prenotazioniRepository.findById(idPrenotazione);
+    }
+
+    public ResponseEntity<Prenotazioni> aggiornaPrenotazioni(PrenotazioniDTO prenotazione){
+        Prenotazioni prenotazioneSalvata = modelMapper.map(prenotazione, Prenotazioni.class);
+        prenotazioniRepository.saveAndFlush(prenotazioneSalvata);
+        return new ResponseEntity<>(prenotazioneSalvata, HttpStatus.CREATED);
+
+    }
+
+    public void cancellaPrenotazione(int idPrenotazione) {
         prenotazioniRepository.deleteById(idPrenotazione);
     }
+
 }
